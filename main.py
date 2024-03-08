5f615db286144169408fb427a2e90ff63cae6c46
Toliva Solomon <tisopar@gmail.com>
2024-03-08 13:08:20 +0300

    feat: enable Alvin to tell time

--- main.py
+++ main.py
@@ -4,43 +4,51 @@ import datetime
 
 
 class Assistant:
-  def __init__(self):
-    self.engine = pyttsx3.init()
-    self.voices = self.engine.getProperty('voices')
-    self.engine.setProperty('voice', self.voices[24].id)
-    self.recognizer = sr.Recognizer()
+    def __init__(self):
+        self.engine = pyttsx3.init()
+        self.voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice', self.voices[24].id)
+        self.recognizer = sr.Recognizer()
 
-  def speak(self, text):
-    self.engine.say(text)
-    self.engine.runAndWait()
+    def speak(self, text):
+        self.engine.say(text)
+        self.engine.runAndWait()
 
-  def listen(self):
-    with sr.Microphone() as source:
-      print("Listening...")
-      audio = self.recognizer.listen(source)
-    try:
-      text = self.recognizer.recognize_google(audio)
-      print(f"I heard: \n{text}")
-      return text.lower()
-    except sr.UnknownValueError:
-      print("Sorry, could not understand audio")
-      return None
-    except sr.RequestError as e:
-      print("Could not request results from Google Speech Recognition service; {0}".format(e))
-      return None
+    def listen(self):
+        with sr.Microphone() as source:
+            print("Listening...")
+            audio = self.recognizer.listen(source)
+        try:
+            text = self.recognizer.recognize_google(audio)
+            print(f"I heard: \n{text}")
+            return text.lower()
+        except sr.UnknownValueError:
+            print("Sorry, could not understand audio")
+            return None
+        except sr.RequestError as e:
+            print(
+                "Could not request results from Google Speech Recognition service; {0}".format(e))
+            return None
+        
+    def get_time(self):
+        now = datetime.datetime.now()
+        current_time = now.strftime("%I:%M %p")
+        return current_time
 
-  def run(self):
-    while True:
-      user_text = self.listen()
-      if user_text:
-        if "alvin" in user_text:
-          self.speak("Hi there! How can I help you?")
-        else:
-          self.speak("Sorry, I'm not yet that advanced!")
+    def run(self):
+        while True:
+            user_text = self.listen()
+            if user_text:
+                if "alvin" in user_text:
+                    self.speak("Hi there! How can I help you?")
+                elif "time" in user_text:
+                    self.speak(f"The currrent time is {self.get_time()}")
+                else:
+                    self.speak("Sorry, I'm not yet that advanced!")
 
 
 if __name__ == "__main__":
-  # Create an instance of the Assistant class
-  alvin = Assistant()
-  # Call the run method to start the assistant
-  alvin.run()
+    # Create an instance of the Assistant class
+    alvin = Assistant()
+    # Call the run method to start the assistant
+    alvin.run()
